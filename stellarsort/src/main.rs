cc73eb0645c4fc9a70d1b282e0c055e4edb81663
Christian Brendlin <ch@brendlinonline.de>
2024-10-06 13:35:03 +0200

    Added denoise

--- stellarsort/src/main.rs
+++ stellarsort/src/main.rs
@@ -1,3 +1,14 @@
+use stellarsort_core::detect_blur;
+
 fn main() {
-    println!("Hello, world!");
+    let img = image::open("blur_noise.png").unwrap();
+
+    let blur_threshold = 100.0;  // Adjust this based on testing
+
+    // Check if the image is blurry
+    if detect_blur(img, blur_threshold, 1.0) {
+        println!("The image is blurry.");
+    } else {
+        println!("The image is sharp.");
+    }
 }
--- stellarsort_core/src/image_analysis.rs
+++ stellarsort_core/src/image_analysis.rs
@@ -1,5 +1,7 @@
 use image::DynamicImage;
+use imageproc::filter::{laplacian_filter, gaussian_blur_f32};
 use thiserror::Error;
+use log::{info, warn, error};
 
 /// Benutzerdefinierter Fehler für die Bildanalyse
 #[derive(Error, Debug)]
@@ -28,18 +30,32 @@ pub fn calculate_variance(image: &imageproc::definitions::Image<image::Luma<i16>
 }
 
 /// Erkennt, ob ein Bild unscharf ist basierend auf der Varianz der Laplacian-Ergebnisse.
-pub fn detect_blur(image: DynamicImage, blur_threshold: f64) -> bool {
+pub fn detect_blur(image: DynamicImage, blur_threshold: f64, denoise_sigma: f32) -> bool {
+    info!("Starting blur detection");
+
     // Schritt 1: In Graustufen konvertieren
     let grayscale_image = image.to_luma8();
 
+    let denoised_image = gaussian_blur_f32(&grayscale_image, denoise_sigma);
+
     // Schritt 2: Laplacian-Filter anwenden
-    let laplacian = imageproc::filter::laplacian_filter(&grayscale_image);
+    let laplacian = laplacian_filter(&denoised_image);
 
     // Schritt 3: Varianz der Laplacian-Ergebnisse berechnen
     let variance = calculate_variance(&laplacian);
 
+    info!("Varianz des Bildes nach Rauschreduktion: {}", variance);
     // Schritt 4: Vergleichen mit dem Schärfe-Schwellenwert
-    variance < blur_threshold
+
+
+    // Schritt 5: Vergleichen mit dem Schärfe-Schwellenwert
+    if variance < blur_threshold {
+        warn!("Bild ist unscharf (Varianz: {})", variance);
+        true
+    } else {
+        info!("Bild ist scharf (Varianz: {})", variance);
+        false
+    }
 }
 
 
@@ -69,7 +85,7 @@ mod tests {
         let blurry_img = ImageBuffer::from_fn(2, 2, |_, _| Luma([10]));
         let dynamic_blurry = DynamicImage::ImageLuma8(blurry_img);
 
-        assert!(detect_blur(dynamic_blurry, 5.0));
+        assert!(detect_blur(dynamic_blurry, 5.0, 1.0));
 
         // Ein Bild mit mehr Kanten (scharf)
         let sharp_img = ImageBuffer::from_fn(2, 2, |x, y| {
@@ -81,6 +97,6 @@ mod tests {
         });
         let dynamic_sharp = DynamicImage::ImageLuma8(sharp_img);
 
-        assert!(!detect_blur(dynamic_sharp, 5.0));
+        assert!(!detect_blur(dynamic_sharp, 5.0, 1.0));
     }
 }
